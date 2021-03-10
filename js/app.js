57c2e215638cce36a9828a346f10ab1f9a3ba5a8
Sadman Sakib Prodhan <sadmansakibprodhan@gmail.com>
2021-03-10 11:51:28 +0600

    First Commit

---
+++ js/app.js
@@ -0,0 +1,36 @@
+$(document).ready(function(){
+
+    $('.food-slider').slick({
+        slidesToShow:3,
+        slidesToScroll:1,
+        prevArrow:".prev-btn",
+        nextArrow:".next-btn",
+        responsive:[
+            {
+                breakpoint:992,
+                settings:{
+                    slidesToShow:2,
+                }
+            },
+
+            {
+                breakpoint:768,
+                settings:{
+                    slidesToShow:1,
+                }
+            }
+        ]
+
+
+    });
+
+
+    $('.nav-trigger').click(function(){
+        $('.site-content-wrapper').toggleClass('scaled');
+        
+
+    })
+
+});
+
+
